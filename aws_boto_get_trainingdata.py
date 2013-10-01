1fa65b44c07c0861e3fcd456046b79c5e2dab335
payoung <paul.andy.young@gmail.com>
2013-10-01 14:34:57 -0700

    Added a python script that fetches training data from S3 for use with EC2 instances

---
+++ aws_boto_get_trainingdata.py
@@ -0,0 +1,27 @@
+import boto
+import boto.s3.connection
+import credentials as cr
+
+# Connect to S3
+conn = boto.connect_s3(
+        aws_access_key_id = cr.access_key,
+        aws_secret_access_key = cr.secret_key,
+        host = 's3-us-west-2.amazonaws.com',
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+        )
+
+# Connect to bucket
+bucket = conn.get_bucket('belkin-data')
+
+# Get the list of Training data sets in S3
+training_data_list = []
+for key in bucket.list():
+    if key.name.find('Tagged') != -1:
+        training_data_list.append(key.name)
+
+# Save datasets to file
+for data in training_data_list:
+    print "Saving %s" % data
+    key = bucket.get_key(data)
+    dataout = 'data/' + data
+    key.get_contents_to_filename(dataout)
