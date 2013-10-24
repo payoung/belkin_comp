c419993270199468759569eaf4f0de921f9df9a8
payoung <paul.andy.young@gmail.com>
2013-10-23 20:21:19 -0700

    added create_data_dict.py to repo

---
+++ create_data_dict.py
@@ -0,0 +1,29 @@
+# reads in np data, saves it to a panda data frame
+import numpy as np
+import pandas as pd
+
+def create_data_dict(filepath):
+    
+    f = file(filepath, "rb")
+
+    d1 = {}
+    d2 = {}
+
+    taggingInfo = np.load(f)
+
+    d1['L1_TimeTicks'] = np.load(f)
+    d1['L1_Real'] = np.load(f)
+    d1['L1_Imag'] = np.load(f)
+    d1['L1_App'] = np.load(f)
+    d1['L1_Pf'] = np.load(f)
+
+    d2['L2_TimeTicks'] = np.load(f)
+    d2['L2_Real'] = np.load(f)
+    d2['L2_Imag'] = np.load(f)
+    d2['L2_App'] = np.load(f)
+    d2['L2_Pf'] = np.load(f)
+
+    #df1 = pd.DataFrame(d1)
+    #df2 = pd.DataFrame(d2)
+
+    return taggingInfo, d1, d2
